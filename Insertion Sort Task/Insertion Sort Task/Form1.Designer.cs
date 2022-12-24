c53cd3850f29dcac3777d2b2bf00a15e5f6aa45a
7aneen <76905760+HaneenElsawaf@users.noreply.github.com>
2022-12-24 22:25:37 +0200

    Add files via upload

---
+++ Insertion Sort Task/Insertion Sort Task/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace Insertion_Sort_Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt11 = new System.Windows.Forms.TextBox();
+            this.txt10 = new System.Windows.Forms.TextBox();
+            this.txt9 = new System.Windows.Forms.TextBox();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.Sorting_button = new System.Windows.Forms.Button();
+            this.clearing_button = new System.Windows.Forms.Button();
+            this.txt12 = new System.Windows.Forms.TextBox();
+            this.txt13 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultlbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU-ExtB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.label1.Location = new System.Drawing.Point(216, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insertion sort";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MingLiU-ExtB", 16F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(35, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(612, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter array\'s numbers: each in a textbox";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt1.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt1.Location = new System.Drawing.Point(56, 136);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(174, 43);
+            this.txt1.TabIndex = 2;
+            this.txt1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt2
+            // 
+            this.txt2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt2.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt2.Location = new System.Drawing.Point(56, 185);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(174, 43);
+            this.txt2.TabIndex = 3;
+            this.txt2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txt3
+            // 
+            this.txt3.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt3.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt3.Location = new System.Drawing.Point(56, 234);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(174, 43);
+            this.txt3.TabIndex = 4;
+            this.txt3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txt4
+            // 
+            this.txt4.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt4.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt4.Location = new System.Drawing.Point(56, 283);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(174, 43);
+            this.txt4.TabIndex = 5;
+            this.txt4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txt11
+            // 
+            this.txt11.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt11.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt11.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt11.Location = new System.Drawing.Point(255, 234);
+            this.txt11.Name = "txt11";
+            this.txt11.Size = new System.Drawing.Size(174, 43);
+            this.txt11.TabIndex = 6;
+            this.txt11.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // txt10
+            // 
+            this.txt10.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt10.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt10.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt10.Location = new System.Drawing.Point(255, 185);
+            this.txt10.Name = "txt10";
+            this.txt10.Size = new System.Drawing.Size(174, 43);
+            this.txt10.TabIndex = 7;
+            this.txt10.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // txt9
+            // 
+            this.txt9.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt9.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt9.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt9.Location = new System.Drawing.Point(255, 136);
+            this.txt9.Name = "txt9";
+            this.txt9.Size = new System.Drawing.Size(174, 43);
+            this.txt9.TabIndex = 8;
+            this.txt9.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // txt8
+            // 
+            this.txt8.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt8.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt8.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt8.Location = new System.Drawing.Point(56, 479);
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(174, 43);
+            this.txt8.TabIndex = 9;
+            this.txt8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // txt7
+            // 
+            this.txt7.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt7.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt7.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt7.Location = new System.Drawing.Point(56, 430);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(174, 43);
+            this.txt7.TabIndex = 10;
+            this.txt7.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // txt6
+            // 
+            this.txt6.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt6.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt6.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt6.Location = new System.Drawing.Point(56, 381);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(174, 43);
+            this.txt6.TabIndex = 11;
+            this.txt6.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // txt5
+            // 
+            this.txt5.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt5.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt5.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt5.Location = new System.Drawing.Point(56, 332);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(174, 43);
+            this.txt5.TabIndex = 12;
+            this.txt5.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // Sorting_button
+            // 
+            this.Sorting_button.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold);
+            this.Sorting_button.ForeColor = System.Drawing.Color.MediumPurple;
+            this.Sorting_button.Location = new System.Drawing.Point(255, 421);
+            this.Sorting_button.Name = "Sorting_button";
+            this.Sorting_button.Size = new System.Drawing.Size(140, 55);
+            this.Sorting_button.TabIndex = 13;
+            this.Sorting_button.Text = "Sort";
+            this.Sorting_button.UseVisualStyleBackColor = true;
+            this.Sorting_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // clearing_button
+            // 
+            this.clearing_button.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold);
+            this.clearing_button.ForeColor = System.Drawing.Color.MediumPurple;
+            this.clearing_button.Location = new System.Drawing.Point(401, 421);
+            this.clearing_button.Name = "clearing_button";
+            this.clearing_button.Size = new System.Drawing.Size(140, 55);
+            this.clearing_button.TabIndex = 14;
+            this.clearing_button.Text = "Reset";
+            this.clearing_button.UseVisualStyleBackColor = true;
+            this.clearing_button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt12
+            // 
+            this.txt12.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt12.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt12.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt12.Location = new System.Drawing.Point(255, 283);
+            this.txt12.Name = "txt12";
+            this.txt12.Size = new System.Drawing.Size(174, 43);
+            this.txt12.TabIndex = 15;
+            this.txt12.TextChanged += new System.EventHandler(this.textBox12_TextChanged);
+            // 
+            // txt13
+            // 
+            this.txt13.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txt13.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt13.ForeColor = System.Drawing.Color.LightGreen;
+            this.txt13.Location = new System.Drawing.Point(255, 332);
+            this.txt13.Name = "txt13";
+            this.txt13.Size = new System.Drawing.Size(174, 43);
+            this.txt13.TabIndex = 16;
+            this.txt13.TextChanged += new System.EventHandler(this.textBox13_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MingLiU-ExtB", 16F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Coral;
+            this.label3.Location = new System.Drawing.Point(471, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 27);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Results:";
+            // 
+            // resultlbl
+            // 
+            this.resultlbl.AutoSize = true;
+            this.resultlbl.Font = new System.Drawing.Font("MingLiU-ExtB", 16F, System.Drawing.FontStyle.Bold);
+            this.resultlbl.ForeColor = System.Drawing.Color.MediumPurple;
+            this.resultlbl.Location = new System.Drawing.Point(497, 239);
+            this.resultlbl.Name = "resultlbl";
+            this.resultlbl.Size = new System.Drawing.Size(0, 27);
+            this.resultlbl.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 557);
+            this.Controls.Add(this.resultlbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt13);
+            this.Controls.Add(this.txt12);
+            this.Controls.Add(this.clearing_button);
+            this.Controls.Add(this.Sorting_button);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.txt8);
+            this.Controls.Add(this.txt9);
+            this.Controls.Add(this.txt10);
+            this.Controls.Add(this.txt11);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt11;
+        private System.Windows.Forms.TextBox txt10;
+        private System.Windows.Forms.TextBox txt9;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.Button Sorting_button;
+        private System.Windows.Forms.Button clearing_button;
+        private System.Windows.Forms.TextBox txt12;
+        private System.Windows.Forms.TextBox txt13;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultlbl;
+    }
+}
+
---
+++ Insertion Sort Task/Insertion Sort Task/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Insertion_Sort_Task
+{
+    public partial class Form1 : Form
+    {
+        public int num1;
+        public int num2;
+        public int num3;
+        public int num4;
+        public int num5;
+        public int num6;
+        public int num7;
+        public int num8;
+        public int num9;
+        public int num10;
+        public int num11;
+        public int num12;
+        public int num13;
+
+        int[] sorting_array = new int[13];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(txt1.Text);
+            num2 = int.Parse(txt2.Text);
+            num3 = int.Parse(txt3.Text);
+            num4 = int.Parse(txt4.Text);
+            num5 = int.Parse(txt5.Text);
+            num6 = int.Parse(txt6.Text);
+            num7 = int.Parse(txt7.Text);
+            num8 = int.Parse(txt8.Text);
+            num9 = int.Parse(txt9.Text);
+            num10 = int.Parse(txt10.Text);
+            num11 = int.Parse(txt11.Text);
+            num12 = int.Parse(txt12.Text);
+            num13 = int.Parse(txt13.Text);
+
+            sorting_array = new int[] { num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, num12, num13};
+
+            PrintIntegerArray(InsertionSort(sorting_array));
+
+            string sorted_array = string.Join(",", sorting_array);
+            resultlbl.Text = sorted_array;
+
+            Console.ReadLine();
+
+            int[] InsertionSort(int[] sortingArray)
+            {
+                for (int i = 0; i < sortingArray.Length - 1; i++)
+                {
+                    for (int j = i + 1; j > 0; j--)
+                    {
+                        if (sortingArray[j - 1] > sortingArray[j])
+                        {
+                            int key = sortingArray[j - 1];
+                            sortingArray[j - 1] = sortingArray[j];
+                            sortingArray[j] = key;
+                        }
+                    }
+
+                }
+                return sortingArray;
+            }
+
+            void PrintIntegerArray(int[] array)
+            {
+                foreach (int i in array)
+                {
+                    Console.Write(i.ToString() + "  ");
+                }
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "";
+            txt2.Text = "";
+            txt3.Text = "";
+            txt4.Text = "";
+            txt5.Text = "";
+            txt6.Text = "";
+            txt7.Text = "";
+            txt8.Text = "";
+            txt9.Text = "";
+            txt10.Text = "";
+            txt11.Text = "";
+            txt12.Text = "";
+            txt13.Text = "";
+
+            sorting_array = new int[13];
+            resultlbl.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
